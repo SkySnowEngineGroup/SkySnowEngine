057219c9a4269fe4f768d0ae353ee59f6becd1bb
liuqian <liuqian@bigo.sg>
2020-11-10 18:30:03 +0800

    Project recompile: Change project name

--- Source/Samples/02_Window/window.cpp
+++ Source/Samples/02_Window/window.cpp
@@ -12,7 +12,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "NuwaEngine", NULL, NULL);
     if(window == NULL)
     {
         std::cout<< "Failed to create GLFW window" << std::endl;
