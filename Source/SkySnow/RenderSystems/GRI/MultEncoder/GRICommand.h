34b1cf950ff72928abe0036a588f503f39f4d896
SkySnow <18712852082@163.com>
2022-06-08 20:30:39 +0800

    update some about multithread encoder framework explain.

--- Source/SkySnow/RenderSystems/GRI/MultEncoder/GRICommand.h
+++ Source/SkySnow/RenderSystems/GRI/MultEncoder/GRICommand.h
@@ -1,6 +1,6 @@
 //
 // Copyright(c) 2020 - 2022 the SkySnowEngine project.
-// Open source is written by wangcan(crygl),liuqian(SkySnow),zhangshuangxue(Calence)
+// Open source is written by liuqian(SkySnow),zhangshuangxue(Calence)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this softwareand associated documentation files(the "Software"), to deal
@@ -21,17 +21,33 @@
 // THE SOFTWARE.
 //
 #pragma once
+//Commandbuffer encoding render commands are encoded in units of one pipeline and submitted in units of one pipeline 
+//Command commit to CommandBuffers,CommandBuffer commit to CommandQueue
+//CommandBufferPool alloc CommandBuffer
+//Commandbuffers are synchronized through events or memory barriers
+//Commandqueues are synchronized between semaphores
+//The CPU and GPU are synchronized through a fence
+//If the same thread has more than one encoder encoding it can have a CommandBufferPool of its own
 class GRICommand
 {
 
 };
 
-class GRICommandPool
+//command encoder
+class GRICommandBuffers
 {
 
 };
-//command encoder
-class GRICommandBuffers
+//Alloc CommandBuffers
+class GRICommandBufferPool
+{
+
+};
+//CommandBuufer commit command to commandQueue
+//Abstract a set of such concepts for OpengL, and some encapsulation for OpengL,
+//so that in the future compatible with Vulkan and Metal, also easier 
+//
+class GRICommandQueue
 {
 
 };
