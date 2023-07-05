dc3af4a5f36a926ccc09492f1644769b3eabde1d
SkySnow <18712856260@163.com>
2023-07-05 22:42:24 +0800

    imageload base fw

---
+++ Source/SkySnow/Resource/Importer/Texture/StbImageLoad.cpp
@@ -0,0 +1,54 @@
+//
+// Copyright(c) 2020 - 2022 the SkySnowEngine project.
+// Open source is written by liuqian(SkySnow),zhangshuangxue(Calence)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this softwareand associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright noticeand this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+#include "StbImageLoad.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "LogAssert.h"
+namespace SkySnow
+{
+    void* StbImageLoad::StbLoadImage(const std::string& filePath)
+    {
+        int width, height, channels;
+        void* cloneData;
+        unsigned char* image_data = stbi_load(filePath.c_str(), &width, &height, &channels, 0);
+        if(image_data == nullptr)
+        {
+            SN_WARN("Load Image(ImagePath:%s) fail.\n",filePath.c_str());
+            return nullptr;
+        }
+        
+        switch (channels)
+        {
+            case STBI_rgb_alpha:
+                break;
+            case STBI_rgb:
+                break;
+            case STBI_grey_alpha:
+                break;
+            case STBI_grey:
+                break;
+            default:
+                break;
+        }
+    }
+}
