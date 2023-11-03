52128765d446a45b458e35d9484119ca7781bbb6
SkySnow <18712856260@163.com>
2023-11-04 00:14:24 +0800

    xml2 for macos

--- Source/ThirdParty/libxml2/lib/xml2Conf.sh
+++ Source/ThirdParty/libxml2/lib/xml2Conf.sh
@@ -1,8 +1,8 @@
 #
 # Configuration file for using the XML library in GNOME applications
 #
-XML2_LIBDIR="-L/Users/SS3D/SkySnowEngine/Source/ThirdParty/libxml2/libxml2-2.9.10/temp/lib"
+XML2_LIBDIR="-L/Users/skysnow/Downloads/Temp/libxml2-2.9.10/temp/lib"
 XML2_LIBS="-lxml2 -lz  -lpthread  -liconv  -lm "
-XML2_INCLUDEDIR="-I/Users/SS3D/SkySnowEngine/Source/ThirdParty/libxml2/libxml2-2.9.10/temp/include/libxml2"
+XML2_INCLUDEDIR="-I/Users/skysnow/Downloads/Temp/libxml2-2.9.10/temp/include/libxml2"
 MODULE_VERSION="xml2-2.9.10"
 
